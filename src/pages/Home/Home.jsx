4d72873e3a22190d25433755074bb3d095afaf2d
wwwbijay <link2bj@gmail.com>
2023-01-30 23:09:51 +0545

    components added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
 import Slider from '../../components/Slider/Slider'
 
 const Home = () => {
   return (    
     <div>
       <Slider />
+      <FeaturedProducts />
     </div>
   )
 }
